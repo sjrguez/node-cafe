1771d80762c5a54399524ec9e47d9d6794d4ffba
Starlinde la cruz <starlinr82@gmail.com>
2018-10-19 09:05:31 -0400

    cambio de variables

--- server/config/config.js
+++ server/config/config.js
@@ -24,7 +24,7 @@ let urlDB
 if (process.env.NODE_ENV == "dev") {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://cafe-user:123456s@ds121456.mlab.com:21456/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 
